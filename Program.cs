50e5925e2fae7dfe79134a16070b34e0eabea854
Pérez <daniellopezperez2005@gmail.com>
2023-04-14 08:41:39 +0200

    nombre

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola,¿como se llama usted?");
 string name;
 name=Console.ReadLine();
 Console.WriteLine("¡Hola," + name + "!");
